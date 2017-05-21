c26f9e05a08dcf53729e6d89593feea2d8c3ceef
Bogdan Makarets <makarecb@gmail.com>
2017-05-22 00:16:57 +0300

    js1-2

---
+++ js2/js/script.js
@@ -0,0 +1,38 @@
+'use stric';
+
+
+var userChName = [];
+
+var name = prompt("Enter the name");
+var name1 = prompt("Enter the name");
+var name2 = prompt("Enter the name");
+var name3 = prompt("Enter the name");
+var name4 = prompt("Enter the name");
+
+
+userChName.push(name);
+userChName.push(name1);
+userChName.push(name2);
+userChName.push(name3);
+userChName.push(name4);
+
+console.log(userChName);
+
+
+var userLogin = prompt("Enter your login");
+
+for (var i = 0; i < userChName.length; i++)
+{
+if (userChName[i] === userLogin) {
+
+  var match = userLogin;
+}
+
+}
+
+if (match) {
+  console.log(userLogin + " вы успешно вощли");
+
+} else {
+  alert("Try again!")
+}
