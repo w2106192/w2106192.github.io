a5d4ebcd89185ee8b9cb852679c8a0066f5b5bef
Bogdan Makarets <makarecb@gmail.com>
2017-06-05 03:04:25 +0300

    js3

---
+++ js3/js/script.js
@@ -0,0 +1,168 @@
+"use strict"
+
+
+
+  var div = document.createElement('h1');
+
+  div.innerHTML = "тест по программированию";
+
+  document.body.appendChild(div);
+
+
+var form = document.createElement('form');
+form.id = "cb"
+form.innerHTML = "1.Вопрос 1"
+
+  document.body.appendChild(form);
+
+  var cbh = document.getElementById('cb');
+      var val = '1';
+      var cap = 'Jan';
+
+      var cb = document.createElement('input');
+      cb.type = 'checkbox';
+      cbh.appendChild(cb);
+      cb.name = val;
+      cb.value = cap;
+      cb.appendChild(document.createTextNode(cap));
+      var cbh = document.getElementById('cb');
+          var val = '1';
+          var cap = 'Jan';
+
+
+
+
+
+
+          var cb = document.createElement('input');
+          cb.type = 'checkbox';
+          cbh.appendChild(cb);
+          cb.name = val;
+          cb.value = cap;
+          cb.appendChild(document.createTextNode(cap));
+          var cbh = document.getElementById('cb');
+              var val = '1';
+              var cap = 'Jan';
+
+              var cb = document.createElement('input');
+              cb.type = 'checkbox';
+              cbh.appendChild(cb);
+              cb.name = val;
+              cb.value = cap;
+              cb.appendChild(document.createTextNode(cap));
+
+
+
+
+              var form = document.createElement('form');
+              form.id = "cb2"
+              form.innerHTML = "2.Вопрос 2"
+
+                document.body.appendChild(form);
+
+
+
+
+
+                  var cbh = document.getElementById('cb2');
+                      var val = '1';
+                      var cap = 'Jan';
+
+                      var cb = document.createElement('input');
+                      cb.type = 'checkbox';
+                      cbh.appendChild(cb);
+                      cb.name = val;
+                      cb.value = cap;
+                      cb.appendChild(document.createTextNode(cap));
+                      var cbh = document.getElementById('cb2');
+                          var val = '1';
+                          var cap = 'Jan';
+
+
+
+
+
+
+                          var cb = document.createElement('input');
+                          cb.type = 'checkbox';
+                          cbh.appendChild(cb);
+                          cb.name = val;
+                          cb.value = cap;
+                          cb.appendChild(document.createTextNode(cap));
+                          var cbh = document.getElementById('cb2');
+                              var val = '1';
+                              var cap = 'Jan';
+
+                              var cb = document.createElement('input');
+                              cb.type = 'checkbox';
+                              cbh.appendChild(cb);
+                              cb.name = val;
+                              cb.value = cap;
+                              cb.appendChild(document.createTextNode(cap));
+
+
+
+                                            var form = document.createElement('form');
+                                            form.id = "cb3"
+                                            form.innerHTML = "3.Вопрос 3"
+
+                                              document.body.appendChild(form);
+
+
+
+
+
+                                                var cbh = document.getElementById('cb3');
+                                                    var val = '1';
+                                                    var cap = 'Jan';
+
+                                                    var cb = document.createElement('input');
+                                                    cb.type = 'checkbox';
+                                                    cbh.appendChild(cb);
+                                                    cb.name = val;
+                                                    cb.value = cap;
+                                                    cb.appendChild(document.createTextNode(cap));
+                                                    var cbh = document.getElementById('cb3');
+                                                        var val = '1';
+                                                        var cap = 'Jan';
+
+
+
+
+
+
+                                                        var cb = document.createElement('input');
+                                                        cb.type = 'checkbox';
+                                                        cbh.appendChild(cb);
+                                                        cb.name = val;
+                                                        cb.value = cap;
+                                                        cb.appendChild(document.createTextNode(cap));
+                                                        var cbh = document.getElementById('cb3');
+                                                            var val = '1';
+                                                            var cap = 'Jan';
+
+                                                            var cb = document.createElement('input');
+                                                            cb.type = 'checkbox';
+                                                            cbh.appendChild(cb);
+                                                            cb.name = val;
+                                                            cb.value = cap;
+                                                            cb.appendChild(document.createTextNode(cap));
+
+
+
+
+
+
+
+
+  var butt = document.createElement('form');
+
+  butt.id = "butt"
+
+  document.body.appendChild(butt);
+
+
+                                                  var but = document.createElement('input');
+                                                      but.type = 'submit';
+                                                      
+                                                        butt.appendChild(but);
